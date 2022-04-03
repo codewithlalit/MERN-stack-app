66c48decd43c93c80a9d6cbabc0a3226227544d2
Lalit <lalitpatil007@outlook.com>
2022-04-03 16:24:53 -0400

    create client app setup

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -16,8 +19,39 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
+//hashing the password to ensure security
+//use traditional function as 'this' keyword will not work with arrow function
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.cPassword = await bcrypt.hash(this.cPassword, 12);
+  }
+  next();
+});
+
+//generating tokens for the user
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Collection creation in database
 const User = mongoose.model("USER", userSchema);
 
 export default User;
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcryptjs";
 //Call User Schema
 import User from "../model/userSchema.js";
 
@@ -7,7 +8,9 @@ const router = express.Router();
 
 // Home Route
 router.get("/", (req, res) => {
-  res.send("Hello !! You are connected to server with router.");
+  res.send(
+    "<h1>Hello !! You are connected to server.</h1><h2>Enjoy the APIs from this server.</h2>"
+  );
 });
 
 // Register Route
@@ -32,7 +35,7 @@ router.post("/register", async (req, res) => {
 
     await user.save();
     return res.status(201).json({ message: "User created successfully" });
-  } catch {
+  } catch (err) {
     console.log(err);
   }
 });
@@ -45,17 +48,36 @@ router.post("/login", async (req, res) => {
     const checkEmail = await User.findOne({ email: email });
 
     if (!checkEmail) {
-      return res.status(400).json({ error: "Email not found !" });
-    }
-    if (password === checkEmail.password) {
-      return res.status(200).json({ message: "User logged in successfully !" });
-    } else {
       return res
         .status(400)
-        .json({ error: "Password Incorrect !! Please try again !!" });
+        .json({ error: "Invalid Credentials !! Please try again !!" });
+    } else {
+      const isPasswordMatched = await bcrypt.compare(
+        password,
+        checkEmail.password
+      );
+
+      //create token for the object received by checkEmail.
+      let token = await checkEmail.generateAuthToken();
+
+      // saving token to the cookie for 5 minutes in milliseconds
+      res.cookie("jwtoken", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 300000),
+      });
+
+      if (isPasswordMatched) {
+        return res
+          .status(200)
+          .json({ message: "User logged in successfully !" });
+      } else {
+        return res
+          .status(400)
+          .json({ error: "Invalid Credentials !! Please try again !!" });
+      }
     }
-  } catch {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 });
 export default router;
