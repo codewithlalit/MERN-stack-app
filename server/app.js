32a5adbeb71660dbcbfa55fa71c6c6fd10c5776e
Lalit <lalitpatil007@outlook.com>
2022-04-03 14:20:29 -0400

    add login functionality

--- server/app.js
+++ server/app.js
@@ -1,15 +1,21 @@
 import express from "express";
 import "dotenv/config";
+//Database connection
 import "./db/connection.js";
-
+//Routes imported
 import auth from "./router/auth.js";
+
 const app = express();
 
+//Use express.json instead of body-parser
 app.use(express.json());
+
+//Use Routes
 app.use(auth);
 
 const PORT = process.env.SERVER_PORT;
 
+//Set-up port
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,10 +1,16 @@
 import express from "express";
+//Call User Schema
 import User from "../model/userSchema.js";
+
+//Use router from express
 const router = express.Router();
 
+// Home Route
 router.get("/", (req, res) => {
-  res.send("Hello from router");
+  res.send("Hello !! You are connected to server with router.");
 });
+
+// Register Route
 router.post("/register", async (req, res) => {
   const { name, email, password, cPassword } = req.body;
 
@@ -28,6 +34,28 @@ router.post("/register", async (req, res) => {
     return res.status(201).json({ message: "User created successfully" });
   } catch {
     console.log(err);
+  }
+});
+
+//Login Route
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const checkEmail = await User.findOne({ email: email });
+
+    if (!checkEmail) {
+      return res.status(400).json({ error: "Email not found !" });
+    }
+    if (password === checkEmail.password) {
+      return res.status(200).json({ message: "User logged in successfully !" });
+    } else {
+      return res
+        .status(400)
+        .json({ error: "Password Incorrect !! Please try again !!" });
+    }
+  } catch {
+    console.log(error);
   }
 });
 export default router;
